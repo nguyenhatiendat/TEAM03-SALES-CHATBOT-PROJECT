82ad3aa50e28fe89412f28c9771f562569cdfdcc
nguyenhatiendat <dat.2474802010077@vanlanguni.vn>
2026-04-05 14:14:41 +0700

    updates

---
+++ NguyenHaTienDat_2474802010077/.firebaseapp/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBydAzunkJI9NVC1M-NmSZWxvOXg8LnoKo",
+  authDomain: "team3-3than-dang-chatbot.firebaseapp.com",
+  projectId: "team3-3than-dang-chatbot",
+  storageBucket: "team3-3than-dang-chatbot.firebasestorage.app",
+  messagingSenderId: "963296403533",
+  appId: "1:963296403533:web:8aa94bd7bea8116ac27c04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
